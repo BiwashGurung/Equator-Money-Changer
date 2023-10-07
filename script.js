b9b3c8975d5c8b3ea767a1773c1379f1dcb50532
BiwashGurung <varungrg10@gmail.com>
2023-10-07 09:48:31 +0545

    using some logics

--- script.js
+++ script.js
@@ -167,4 +167,24 @@ let country_list = {
     toCurrency = document.querySelector(".to select"),
     getButton = document.querySelector("form button");
   
+  for (let i = 0; i < dropList.length; i++) {
+    for (let currency_code in country_list) {
+      let selected =
+        i == 0
+          ? currency_code == "USD"
+            ? "selected"
+            : ""
+          : currency_code == "NPR"
+          ? "selected"
+          : "";
+      let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
+      dropList[i].insertAdjacentHTML("beforeend", optionTag);
+    }
+    dropList[i].addEventListener("change", (e) => {
+      loadFlag(e.target);
+    });
+  }
+  
+
+ 
   
